cda6a202b248be51fa111856f394170bca720b06
Paweł <heropn754@gmail.com>
2022-12-14 20:24:10 +0100

    Added toolbar and basic menu, migrated to AndroidX to support newest version of toolbar

--- Application/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Application/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,10 @@ package com.example.myapplication
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.view.Menu
 import android.view.View
+import android.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -14,16 +17,25 @@ class MainActivity : AppCompatActivity() {
     private var toDoFragment = Table()
     private var inProgressFragment = Table()
     private var doneFragment = Table()
+    private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.board)
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.toDoTableFragment, toDoFragment)
-            replace(R.id.inProgressTableFragment,inProgressFragment)
-            replace(R.id.doneTableFragment, doneFragment)
-            addToBackStack(null)
-            commit()
-        }
+        setContentView(R.layout.activity_main)
+        toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        //ShowPopUpMenu(window.decorView.findViewById(android.R.id.content))
+        //supportFragmentManager.beginTransaction().apply{
+        //    replace(R.id.toDoTableFragment, toDoFragment)
+        //    replace(R.id.inProgressTableFragment,inProgressFragment)
+        //    replace(R.id.doneTableFragment, doneFragment)
+        //    addToBackStack(null)
+        //    commit()
+        //}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.popup_menu, menu)
+        return true
     }
 }
