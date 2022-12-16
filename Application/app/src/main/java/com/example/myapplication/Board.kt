ffb9b4ab0c293c5e6dbf10279caa5f8ee37e5ff7
Paweł <heropn754@gmail.com>
2022-12-16 23:24:05 +0100

    Added navigation between lougout menu and login fragment

--- Application/app/src/main/java/com/example/myapplication/Board.kt
+++ Application/app/src/main/java/com/example/myapplication/Board.kt
@@ -1,20 +1,33 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 class Board : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        Toast.makeText(context, "View Created", Toast.LENGTH_SHORT).show()
         val view: View = inflater.inflate(R.layout.board, container, false)
         return view
     }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.Logout -> {
+                Navigation.findNavController(requireView()).navigate(R.id.action_board_to_login)
+                true
+            }
+        else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- Application/app/src/main/java/com/example/myapplication/Login.kt
+++ Application/app/src/main/java/com/example/myapplication/Login.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 
@@ -20,10 +21,23 @@ class Login : Fragment() {
         val loginButton = view.findViewById<Button>(R.id.loginButton)
 
         loginButton.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_login_to_board)
+            TryToLogin();
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun TryToLogin()
+    {
+        if (false)
+        {
+            Navigation.findNavController(requireView()).navigate(R.id.action_login_to_board)
+        }
+        else
+        {
+            Toast.makeText(context, "Invalid username or passowrd", Toast.LENGTH_LONG).show()
+        }
     }
 }
